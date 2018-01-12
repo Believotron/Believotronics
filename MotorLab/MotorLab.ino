acb4c14c562e89fd9d58fda195a9bb6fd2c44994
Daniel D Lindmark <daniel.dee@gmail.com>
2018-01-11 18:55:16 -0600

    Bug keeping motor from moving. WIP 4 motor, 2 Joy infrastructure.

--- MotorLab/MotorLab.ino
+++ MotorLab/MotorLab.ino
@@ -8,12 +8,26 @@ struct Motor
     uint8_t  iSignalAddress[NUM_MOTOR_SIGNALS];
     uint16_t iStepRatio;
     int8_t   iStep;
-    uint8_t  bDirection;
+    int8_t   iDirection;
 };
 
 
 Motor motors[NUM_MOTOR_CHANNELS];
 //uint8_t motor[NUM_MOTOR_CHANNELS][NUM_MOTOR_SIGNALS];
+
+struct Joystick
+{
+    uint8_t iXAddr;
+    uint8_t iYAddr;
+    uint16_t iXVal;
+    uint16_t iYVal;
+    uint8_t xMotor;
+    uint8_t yMotor;
+};
+
+#define NUM_JOYS 2
+Joystick joy[NUM_JOYS];
+
 
 
 
@@ -35,15 +49,37 @@ void setup()
     pinMode(ledPin, OUTPUT); // declare the ledPin as an OUTPUT:
     Serial.begin(9600);
 
+    joy[0].iXAddr = A1;
+    joy[0].iYAddr = A0;
+    joy[0].xMotor = 1;
+    joy[0].yMotor = 0;
+
+    joy[1].iXAddr = A3;
+    joy[1].iYAddr = A2;
+    joy[1].xMotor = 3;
+    //joy[1].yMotor = 4;
+
     motors[0].iSignalAddress[0] = A4;
     motors[0].iSignalAddress[1] = A5;
     motors[0].iSignalAddress[2] = 23;
     motors[0].iSignalAddress[3] = 20;
     motors[0].iStepRatio        = 1;
-    motors[0].bDirection        = 1;
+    motors[0].iDirection        = 0;
     motors[0].iStep             = 0;
 
-    for (int i=0; i< NUM_MOTOR_SIGNALS; i++) {pinMode(motors[0].iSignalAddress[i], OUTPUT);}
+    motors[1].iSignalAddress[0] = 24;
+    motors[1].iSignalAddress[1] = 21;
+    motors[1].iSignalAddress[2] = 1;
+    motors[1].iSignalAddress[3] = 0;
+    motors[1].iStepRatio        = 1;
+    motors[1].iDirection        = 0;
+    motors[1].iStep             = 0;
+
+    for (int iMotors=0; iMotors < 2; iMotors++)
+    {
+            for (int i=0; i< NUM_MOTOR_SIGNALS; i++) {pinMode(motors[iMotors].iSignalAddress[i], OUTPUT);}
+    }
+
 
     digitalWrite(ledPin, HIGH);     // turn the ledPin on
     iGlobalStep = 0;
@@ -52,9 +88,9 @@ void setup()
 int debugJoyXY(int iX, int iY)
 {
     Serial.print("JoyX: [");
-    Serial.print(JoyXVal);
+    Serial.print(iX);
     Serial.print("], JoyY: [");
-    Serial.print(JoyYVal);
+    Serial.print(iY);
     Serial.print("\n");
 
     return 0;
@@ -134,33 +170,87 @@ int debugJoyXY()
     return 0;
 }
 
+int debugFoo(int foo1, int foo2)
+{
+    Serial.print("Foo1: [");
+    Serial.print(foo1);
+    Serial.print("], Foo2: [");
+    Serial.print(foo2);
+    Serial.print("\n");
+
+    return 0;
+}
+
+
+void PollJoysticks()
+{
+    for (int i=0; i<2; i++)
+    {
+        joy[i].iXVal = analogRead(joy[i].iXAddr);
+        joy[i].iYVal = analogRead(joy[i].iYAddr);
+    }
+
+}
+
 void loop()
 {
+    PollJoysticks();
 
-    JoyXVal = analogRead(JoyX);
+    int i= 0;
+    int iMotor = 1;
+    if      (joy[i].iXVal > 900) { motors[iMotor].iDirection =  1; }
+    else if (joy[i].iXVal < 432) { motors[iMotor].iDirection = -1; }
+    else                         { motors[iMotor].iDirection =  0; }
 
-    if (JoyXVal > 900)
+    iMotor = 0;
+    if      (joy[i].iYVal > 900) { motors[iMotor].iDirection =  1; }
+    else if (joy[i].iYVal < 432) { motors[iMotor].iDirection = -1; }
+    else                         { motors[iMotor].iDirection =  0; }
+
+    i = 1; iMotor = 2;
+    if      (joy[i].iYVal > 900) { motors[iMotor].iDirection =  1; }
+    else if (joy[i].iYVal < 432) { motors[iMotor].iDirection = -1; }
+    else                         { motors[iMotor].iDirection =  0; }
+
+    iMotor = 3;
+    if      (joy[i].iYVal > 900) { motors[iMotor].iDirection = -1; }
+    else if (joy[i].iYVal < 432) { motors[iMotor].iDirection =  1; }
+    else                         { motors[iMotor].iDirection =  0; }
+
+    for (int i=0; i<1; i++)
     {
-        motors[0].bDirection = 1;
-        if (++(motors[0].iStep) >=8){ motors[0].iStep = 0; }
-        SetMotor(0, motors[0].iStep);
+        if (motors[i].iDirection !=0)
+        {
+
+            if      (motors[i].iDirection > 0) { if (++(motors[iMotor].iStep) >=8){ motors[iMotor].iStep = 0; } }
+            else if (motors[i].iDirection < 0) { if (--(motors[iMotor].iStep) < 0){ motors[iMotor].iStep = 7; } }
+
+            debugFoo(motors[iMotor].iStep, motors[i].iDirection);
+
+            SetMotor(iMotor, motors[iMotor].iStep);
+        }
+    }
+    delayMicroseconds(2000);
+
+/*
+    if (joy[0].iXVal > 900)
+    {
+        motors[iMotor].iDirection = 1;
+
+        SetMotor(iMotor, motors[iMotor].iStep);
         delayMicroseconds(1000);
     }
-    else if (JoyXVal < 412)
+    else if (joy[0].iXVal < 412)
     {
-        motors[0].bDirection = 0;
-        if (--(motors[0].iStep) < 0){ motors[0].iStep = 7; }
-        SetMotor(0, motors[0].iStep);
+        motors[iMotor].iDirection = -1;
+        if (--(motors[iMotor].iStep) < 0){ motors[iMotor].iStep = 7; }
+        SetMotor(iMotor, motors[iMotor].iStep);
         delayMicroseconds(1000);
     }
+    else { motors[iMotor].iDirection = 0; }
+*/
 
-debugJoyXY();
-
-
-
-
-
-
+//debugJoyXY();
 
     // if  (motors[0].bDirection == 1) { if (++(motors[0].iStep) >=8){ motors[0].iStep = 0; } }
     // else                            { if (--(motors[0].iStep) < 0){ motors[0].iStep = 7; } }
